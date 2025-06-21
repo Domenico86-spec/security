798bbd681ef907a43e191c35eb2afba67c65ce03
Domenico Lobusto <domenico.lobusto86@gmail.com>
2025-06-21 22:20:23 +0200

    salvate prime modifiche

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,11 @@
+<x-layout>
+    <div class="container">
+        <div class="row justify-content-center mt-5">
+            <div class="col-12 text-center">
+                <h1>Register</h1>
+            </div>
+
+        </div>
+
+    </div>
+</x-layout>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+    <h1 class="text-center mt-3">Security</h1>
+</x-layout>
