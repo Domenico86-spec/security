b4be954c8abd904a58a8b879828f486011114187
Domenico Lobusto <domenico.lobusto86@gmail.com>
2025-06-23 16:29:57 +0200

    autenticazione salvata

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,7 +2,46 @@
     <div class="container">
         <div class="row justify-content-center mt-5">
             <div class="col-12 text-center">
-                <h1>Register</h1>
+                <h1>Registrati</h1>
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="container my-5">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-8">
+                <form method="POST" action="{{route('register')}}">
+                @csrf
+                
+                
+                
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Nome utente</label>
+                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="name"></label>
+
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Email</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email">
+
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" name="password">
+
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Conferma Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" name="password_confirmation">
+
+                </div>
+                    <button type="submit" class="btn btn-primary">Registrati</button>
+
+                
+            </form>
+
             </div>
 
         </div>
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -5,19 +5,46 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="{{route('homepage')}}">Home</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
+        @guest
+            
         
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+        
+        <li class="nav- item">
+          <a class="nav-link" href="{{route('register')}}">Registrati</a>
+            </li>
+            <li class="nav- item">
+          <a class="nav-link" href="{{route('login')}}">Login</a>
+            </li>
+
+        @endguest
+          
+        @auth
+
+            <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+           Benvenuto {{Auth::user()->name}} 
+          </a>
+          <ul class="dropdown-menu">
+
+            <li>
+              <a class="dropdown-item" href="#" onclick="event.preventDefault(); document.querySelector('#logout').submit();">Logout</a></li>
+            <form method="POST"  action="{{route('logout')}}" id="logout">@csrf</form>
+           
+            <li><hr class="dropdown-divider"></li>
+          </ul>
+        </li>
+            
+        @endauth
+
+        </ul>
+        
+      
+      
     </div>
   </div>
 </nav>
